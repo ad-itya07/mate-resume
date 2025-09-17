29e4559f39ea330c57706d09ce0a3129dfc1c72c
ad_itya07 <jhaaditya2207@gmail.com>
2025-09-17 22:33:48 +0530

    feat: update PostCSS configuration to use ES module syntax

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,8 @@
-// postcss.config.js
-module.exports = {
-  plugins: [
-    require('@tailwindcss/postcss'),
-    require('autoprefixer'),
-  ],
+// postcss.config.mjs
+
+export default {
+  plugins: {
+    '@tailwindcss/postcss': {},
+    'autoprefixer': {},
+  },
 };
